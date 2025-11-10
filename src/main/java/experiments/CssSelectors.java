29864f569622c4c6277bcb81397255a34b2fb2bd
Marta M <mitrakhovichmarta@gmail.com>
2025-11-10 19:19:09 +0200

    CssSelectors

--- src/main/java/experiments/CssSelectors.java
+++ src/main/java/experiments/CssSelectors.java
@@ -1,6 +1,6 @@
 package experiments;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -23,6 +23,53 @@ public class CssSelectors {
         driver.quit();
 
     }
+
+        @Test
+        public void selectorsHomePageNext(){
+        driver.get("https://demoqa.com/");
+        driver.manage().window().maximize();
+        WebElement footer = driver.findElement(By.tagName("footer"));
+        System.out.println(footer.getTagName());
+        WebElement divElements = driver.findElement(By.cssSelector(".card-up"));
+        divElements.click();
+        WebElement radioButton = driver.findElement(By.cssSelector("#item-2"));
+        radioButton.click();
+        WebElement radioButtonYes = driver.findElement(By.cssSelector("label[for='yesRadio']"));
+        radioButtonYes.click();
+        pause(3);
+        driver.navigate().back();
+        driver.navigate().back();
+
+        hideBanner();
+
+
+        WebElement divWidgets = driver.findElement(By.cssSelector("div[class='category-cards'] [class='card mt-4 top-card']:nth-child(4)"));
+        divWidgets.click();
+        pause(3);
+        driver.navigate().back();
+        pause(3);
+        WebElement divBookStore = driver.findElement(By.cssSelector("div[class='category-cards'] [class='card mt-4 top-card']:last-child"));
+
+        divBookStore.click();
+
+
+
+
+        pause(3);
+        driver.quit();
+
+
+
+
+
+
+
+
+
+        }
+
+
+
     public void pause(int time){
         try {
             Thread.sleep(4000);
@@ -30,5 +77,11 @@ public class CssSelectors {
             throw new RuntimeException(e);
         }
     }
+    public void hideBanner(){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("document.querySelector('#fixedban').style.display = 'none'");
 
+
+
+    }
 }
