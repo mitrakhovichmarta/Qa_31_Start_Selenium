44e64425efa638e9adf4b8a22af5660dd6845c3e
Marta M <mitrakhovichmarta@gmail.com>
2025-11-10 22:01:55 +0200

    HW_10_11_Marta

--- src/test/java/HomeWorkMarta.java
+++ src/test/java/HomeWorkMarta.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -11,25 +13,37 @@ public class HomeWorkMarta {
     public void setUp(){
         wd = new ChromeDriver();
         wd.get("https://telranedu.web.app/login");
+        wd.manage().window().maximize();
 
 
     }
     @Test
     public void locators () {
         //by tagName
-
+        //WebElement buttonSubmit = wd.findElement(By.tagName("login"));
+        WebElement buttonSubmit1 = wd.findElement(By.cssSelector("[name= 'login']"));
 
         //by class
-
+        WebElement classContainer1 = wd.findElement(By.cssSelector(".container"));
 
         //by id
+        WebElement root = wd.findElement(By.cssSelector("#root"));
 
 
         //by attribute
-
-
+        WebElement home = wd.findElement(By.cssSelector("[href='/home']"));
+        WebElement about = wd.findElement(By.cssSelector("[href='/about']"));
+        WebElement login = wd.findElement(By.cssSelector("[href='/login']"));
 
         // one of elements find by - start - end -contains
+        //start
+        WebElement email = wd.findElement(By.cssSelector("[placeholder ^= 'Email']"));
+        //end
+        WebElement a5 = wd.findElement(By.cssSelector("[placeholder $='Password']"));
+        //contains
+        WebElement password = wd.findElement(By.cssSelector("[placeholder *='Password']"));
+
+       wd.quit();
 
     }
 
